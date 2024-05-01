42409ac3d66a1aba10041a36a3905ffb340c2420
Daniele Messina <messina.dev@gmail.com>
2024-05-01 22:26:30 +0200

    * add dialog to confirm delete

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,11 +1,27 @@
+import React from "react";
 import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
-import { FAB, Card } from "react-native-paper";
+import { FAB, Card, Portal, Dialog, Button } from "react-native-paper";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteNote, fetchNotes } from "../Store/noteSlice";
 import { useEffect } from "react";
 
 export default function HomeScreen({ navigation }) {
+  const [visible, setVisible] = React.useState(false);
+  const [noteSelected, setNodeSelected] = React.useState(null);
   const notes = useSelector((state) => state.note.notes);
+
+  function showDialog(note) {
+    setNodeSelected(note);
+    setVisible(true);
+  }
+  function hideDialog() {
+    setNodeSelected(null);
+    setVisible(false);
+  }
+  function delNote(id) {
+    dispatch(deleteNote(id));
+    hideDialog();
+  }
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -15,17 +31,13 @@ export default function HomeScreen({ navigation }) {
   const cards =
     notes.map((n) => {
       return (
-        <Card style={styles.card}>
+        <Card style={styles.card} onLongPress={() => showDialog(n)}>
           <Card.Title title={n.title} />
           <Card.Content>
             <Text>{n.text}</Text>
           </Card.Content>
           <Card.Actions>
-            <FAB
-              icon="delete"
-              size="small"
-              onPress={() => dispatch(deleteNote(n.id))}
-            />
+            <FAB icon="delete" size="small" onPress={() => showDialog(n)} />
             <FAB icon="pencil" size="small" onPress={() => alert("edit")} />
           </Card.Actions>
         </Card>
@@ -35,6 +47,25 @@ export default function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       {cards}
+
+      <Portal>
+        <Dialog
+          visible={visible}
+          dismissable={false}
+          dismissableBackButton={false}
+        >
+          <Dialog.Title>Delete note</Dialog.Title>
+          <Dialog.Content>
+            <Text variant="bodyMedium">
+              {`Are you sure to delete the selected element "${noteSelected?.title}"?`}
+            </Text>
+          </Dialog.Content>
+          <Dialog.Actions style={styles.cardActions}>
+            <Button onPress={hideDialog}>Cancel</Button>
+            <Button onPress={() => delNote(noteSelected.id)}>Delete</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
 
       <TouchableOpacity style={styles.addButton}>
         <FAB icon="plus" onPress={() => navigation.navigate("Note")} />
@@ -55,5 +86,9 @@ const styles = StyleSheet.create({
   },
   card: {
     marginBottom: 20,
+  },
+  cardActions: {
+    display: "flex",
+    justifyContent: "space-between",
   },
 });
