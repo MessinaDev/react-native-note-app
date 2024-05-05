fc2732408bfa5a9417a1ef1b0dee68a279476e78
Daniele Messina <messina.dev@gmail.com>
2024-05-05 20:56:17 +0200

    add package and category value

--- src/Screens/DetailsNote.js
+++ src/Screens/DetailsNote.js
@@ -3,13 +3,27 @@ import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { FAB, TextInput } from "react-native-paper";
 import { addNote, editNote } from "../Store/noteSlice";
 import { useDispatch } from "react-redux";
+import DropDown from "react-native-paper-dropdown";
 
 export default function DetailsNote({ navigation, route }) {
   const note = { ...route.params?.note };
   const [title, setTitle] = React.useState(note?.title || "");
   const [text, setText] = React.useState(note?.text || "");
+  const [category, setCategory] = React.useState("");
+  const [openDropDown, setOpenDropDown] = React.useState(false);
 
   const dispatch = useDispatch();
+
+  const categories = [
+    {
+      label: "Work",
+      value: "work",
+    },
+    {
+      label: "Personal",
+      value: "personal",
+    },
+  ];
 
   function save() {
     if (title && text) {
@@ -42,8 +56,18 @@ export default function DetailsNote({ navigation, route }) {
         mode="outlined"
         value={title}
         onChangeText={(title) => setTitle(title)}
+        style={{ marginBottom: 10 }}
       ></TextInput>
-      {/* category */}
+      <DropDown
+        label="Category"
+        mode="outlined"
+        visible={openDropDown}
+        showDropDown={() => setOpenDropDown(true)}
+        onDismiss={() => setOpenDropDown(false)}
+        list={categories}
+        value={category}
+        setValue={setCategory}
+      ></DropDown>
       <TextInput
         label="Text"
         mode="outlined"
